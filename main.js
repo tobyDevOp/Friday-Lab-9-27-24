061b8f63059e8accdfb3838bfe3c843662b943ec
tobygoetz <goetz.tobiah@student.greenriver.edu>
2024-09-27 16:07:55 -0700

    installed additional dependencies and updated package.json

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import lodash from 'lodash';
 import chalk from 'chalk'; 
+import colors from 'colors';
+import { faker } from '@faker-js/faker';
+import validator from 'validator';
 
 const array = [1, 4, 2, 3, 1, 4, 5, 2, 3, 2, 1];
 const uniqueNums = lodash.uniq(array);
